7aba2a2878fadf87842923fe367a0c07011f7bd7
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-03-16 20:47:31 +0530

    selector family

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { RecoilRoot, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil"; //useREcoilValue
 import { todosAtomFamily } from "./atoms";
+
+// import { todosAtomFamily } from "./components/store/atoms";
 
 function App() {
   return (
     <RecoilRoot>
-    <Todo id={1}/>
-    <Todo id={2}/>
+      <Todo id={1} />
+      <Todo id={2} />
     </RecoilRoot>
-    );
+  );
 }
 
-function Todo({id}){
+function Todo({ id }) {
+  const currentTodo = useRecoilValue(todosAtomFamily(id));
+  //this id for atom family next introducing for selector family
 
-  const currentTodo = useRecoilValue(todosAtomFamily(id))
-  return(
+  // const [todo,setTodo] = useRecoilState(todosAtomFamily(id))
+  return (
     <div>
-    {currentTodo.title}
-    {currentTodo.description}
+      {currentTodo.title}
+      {currentTodo.description}
     </div>
-  )
+  );
 }
+// {todo.title}
+// {todo.description}
+//{currentTodo.title}
+//{currentTodo.description}
 
 export default App;
